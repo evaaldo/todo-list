534919b0dde3ec6cc5e4f8a42d522e71e63a28d7
Evaldo Rodrigues <110418142+evaaldo@users.noreply.github.com>
2022-10-10 19:16:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,60 @@
 const form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
+const itens = JSON.parse(localStorage.getItem('itens')) || []
+
+itens.forEach((elemento) => {
+    criaElemento(elemento)
+})
 
 form.addEventListener('submit', (evento) => {
 
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['atividade'].value)
+    const atividade = evento.target.elements['atividade']
+
+    const itemAtual = {
+        'atividade': atividade.value
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem('itens', JSON.stringify(itens))
+
+    atividade.value = ''
 
 })
 
-function criaElemento(atividade) {
+function criaElemento(item) {
 
     const novoItem = document.createElement('li')
     novoItem.classList.add('item')
 
-    const removeItem = document.createElement('input')
-    removeItem.classList.add('cadastrar')
-    removeItem.classList.add('remover')
-    removeItem.value += '-'
+    novoItem.innerHTML += item.atividade
 
-    novoItem.innerHTML += atividade.toUpperCase()
+    novoItem.appendChild(botaoDeleta())
 
-    novoItem.appendChild(removeItem)
     lista.appendChild(novoItem)
 
+}
+
+function botaoDeleta(id) {
+
+    const elementoBotao = document.createElement('button')
+    elementoBotao.innerText = 'x'
+    elementoBotao.classList.add('remover')
+
+    elementoBotao.addEventListener('click', function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+
+}
+
+function deletaElemento(elemento, id) {
+    elemento.remove()
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+    localStorage.setItem('itens', JSON.stringify(itens))
 }
